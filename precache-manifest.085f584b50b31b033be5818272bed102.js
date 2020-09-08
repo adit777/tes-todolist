2076dfbf2ce4c305fadbb49f7b6e89629b2d45c1
adit777 <aditya.aulia09@gmail.com>
2020-09-08 15:43:02 +0700

    Updates

---
+++ precache-manifest.085f584b50b31b033be5818272bed102.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08eb0412feab54ab089b93ae7bdbe17",
+    "url": "/adit777/tes-todolist/index.html"
+  },
+  {
+    "revision": "d540fc222c0cdaeea4aa",
+    "url": "/adit777/tes-todolist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36f6871b2c0ca71f779a",
+    "url": "/adit777/tes-todolist/static/js/2.32ef7ac1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adit777/tes-todolist/static/js/2.32ef7ac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d540fc222c0cdaeea4aa",
+    "url": "/adit777/tes-todolist/static/js/main.63e20a3b.chunk.js"
+  },
+  {
+    "revision": "1e802669c7cfdbf8e88b",
+    "url": "/adit777/tes-todolist/static/js/runtime-main.7bd7d76f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/adit777/tes-todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
